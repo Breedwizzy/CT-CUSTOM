272bc79c9dcd436eedc36c3902197a3754611aed
Breedwizzy <breedwizzy@yahoo..com>
2023-10-25 07:08:59 +0100

    FIRST COMMIT

---
+++ front-page.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Blog Site Template</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Blog Site Template">
+    <meta name="author" content="https://youtube.com/FollowAndrew">
+    <link rel="shortcut icon" href="images/logo.png">
+
+    <?php
+    wp_head();
+    ?>
+
+</head>
+
+<body>
+
+
+    <header>
+        <div class='nav-updiv'>
+            &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<span>CALL
+                US
+                NOW!</span>&emsp;&emsp;&emsp;<span
+                class="highlighted">385.154.11.28.35</span>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+            &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;LOGIN&emsp;&emsp;<span
+                class='sign-out'>SIGNOUT</span>
+
+        </div>
+        </br>
+
+
+
+        <nav>
+
+            <ul>
+                <li><a href="#">Title1</a></li>
+                <li><a href="#">Title2</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropbtn">Title3</a>
+                    <div class="dropdown-content">
+                        <a href="#">Submenu1</a>
+                        <a href="#">Submenu2</a>
+                        <div class="sub-dropdown">
+                            <a href="#">Submenu3</a>
+                            <div class="sub-sub-dropdown">
+                                <a href="#">Submenu1</a>
+                                <a href="#">Submenu2</a>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li><a href="#">Title4</a></li>
+                <li class="active"><a href="#">Title5</a></li> <!-- Add the 'active' class here -->
+            </ul>
+            &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+            <span class="your-logo">YOUR</span><span class='logo-your'>LOGO</span>
+        </nav>
+
+    </header>
+
+    </br></br>
+    </br>
+    </br>
+    &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+    <span class='home-we'>Home / Who We are / </span><span class='connn'>Contact </span>
+
+    </br></br>
+    &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+    <span class='connn2'>Contact </span>
+    </br>
+    &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+    Loren ipsum dolar sit amet, consectetur adipiscing elit. aliauam posuere ipsum nec velit mattis elementum. Cum
+    sociis natoque
+    </br>
+    &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+    penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas eu placerat metus, eget placerat libero.
+    </br>
+    </br>
+    </br>
+    &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+    <span class='contact-us-sp'>CONTACT US</span>
+    &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+    <span class='reach-us'>REACH US</span>
+
+
+
+
+
+    </div>
+
+    <?php
+    wp_footer();
+    ?>
+
+
+    <script type='text/javascript'>
+
+        function myFunction() {
+            var navbar = document.getElementById('navul-id');
+            navbar.classList.toggle('show');
+        }
+
+    </script>
+
+
+</body>
+
+</html>
